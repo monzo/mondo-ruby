6425e8368b948c492ddd1cf4548136f8a6e4dc1c
Tom Blomfield <tomblomfield@gmail.com>
2015-11-13 16:53:58 +0000

    attachment file

---
+++ lib/mondo/attachment.rb
@@ -0,0 +1,22 @@
+module Mondo
+  class Attachment < Resource
+    attr_accessor :id, :user_id, :external_id, :file_url, :file_type
+    
+    date_accessor :created
+
+
+    def register
+    	raise  ClientError.new("You have already registered this attachment") unless self.id.nil?
+
+    	self.client.api_post("attachment/register", registration_data)
+    end
+
+    def registration_data
+      {
+        external_id: self.external_id,
+        file_url: self.file_url,
+        file_type: self.file_type, 
+      }
+    end
+  end
+end
