2e8c053a8dec6437e2262c71a173b1e7af3c5dd3
Scott Robertson <scottymeuk@gmail.com>
2016-01-30 23:38:50 +0000

    Return sort code and account number for account

--- lib/mondo/account.rb
+++ lib/mondo/account.rb
@@ -1,7 +1,7 @@
 module Mondo
   class Account < Resource
 
-    attr_accessor :id, :description, :raw_data
+    attr_accessor :id, :description, :raw_data, :sort_code, :account_number
 
     date_accessor :created
   end
