aac35b0cec76cceeb906dfd3aa7bc4e886832a31
Scott Robertson <scottymeuk@gmail.com>
2016-01-30 23:38:56 +0000

    Return balance for account

--- lib/mondo/account.rb
+++ lib/mondo/account.rb
@@ -4,5 +4,9 @@ module Mondo
     attr_accessor :id, :description, :raw_data, :sort_code, :account_number
 
     date_accessor :created
+
+    def balance
+      self.client.balance(id)
+    end
   end
 end
