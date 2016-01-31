8232cd191df1a4254f22d4825d9e16bd23ffc961
Scott Robertson <scottymeuk@gmail.com>
2016-01-31 00:40:45 +0000

    Account id is required, and so is transaction id

--- lib/mondo/client.rb
+++ lib/mondo/client.rb
@@ -127,6 +127,8 @@ module Mondo
     # @method transaction
     # @return <Transaction> of the transaction information
     def transaction(transaction_id, opts = {})
+      raise ClientError.new("You must provide an account id to query transactions") unless self.account_id
+      raise ClientError.new("You must provide an transaction id to query transactions") unless transaction_id
       resp = api_get("/transactions/#{transaction_id}", opts)
       return resp unless resp.error.nil?
       Transaction.new(resp.parsed['transaction'], self)
