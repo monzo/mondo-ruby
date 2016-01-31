3d93c75ec0dc294274a2073fd32b02e69c1ec28a
Scott Robertson <scottymeuk@gmail.com>
2016-01-31 00:40:58 +0000

    Allow access to formatted addresses

--- lib/mondo/address.rb
+++ lib/mondo/address.rb
@@ -1,6 +1,6 @@
 module Mondo
   class Address < Resource
     attr_accessor :address, :city, :region, :country, :postcode, :latitude,
-      :longitude, :raw_data
+      :longitude, :raw_data, :short_formatted, :formatted
   end
 end
