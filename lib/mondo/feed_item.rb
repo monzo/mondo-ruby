d188fb6273b732eae0151a1efa26c9693be195ad
Sam Mason <samm@lostmy.name>
2016-03-22 15:53:26 +0000

    Add title_color and body_color as available options

--- lib/mondo/feed_item.rb
+++ lib/mondo/feed_item.rb
@@ -6,7 +6,9 @@ module Mondo
                   :url, # TODO - make me consistent when the Mondo API changes
                   :background_color,
                   :body,
-                  :type
+                  :type,
+                  :title_color,
+                  :body_color
 
 
     # temporary fix until the API accepts JSON data in the request body
@@ -23,7 +25,9 @@ module Mondo
           title: self.title,
           image_url: self.image_url,
           background_color: self.background_color,
-          body: self.body
+          body: self.body,
+          title_color: self.title_color,
+          body_color: self.body_color
         }
       }
     end
