c9320cd6d5a43713c031f93aafaaa58b1437fa9e
Sam Mason <samm@lostmy.name>
2016-03-22 16:00:03 +0000

    Use Active Support symbolize keys and only require that

--- lib/mondo/utils.rb
+++ lib/mondo/utils.rb
@@ -12,18 +12,5 @@ module Mondo
     def underscore(str)
       str.gsub(/(.)([A-Z])/) { "#{$1}_#{$2.downcase}" }.downcase
     end
-
-    # Hash Helpers
-    def symbolize_keys(hash)
-      symbolize_keys! hash.dup
-    end
-
-    def symbolize_keys!(hash)
-      hash.keys.each do |key|
-        sym_key = key.to_s.to_sym rescue key
-        hash[sym_key] = hash.delete(key) unless hash.key?(sym_key)
-      end
-      hash
-    end
   end
 end
