79a27273249a6465295ec588b427c998a5290124
Bilawal Hameed <bilawal@studenthack.com>
2016-04-06 11:53:59 +0100

    factorised FeedItem

--- lib/mondo/feed_item.rb
+++ lib/mondo/feed_item.rb
@@ -1,29 +1,27 @@
 module Mondo
   class FeedItem < Resource
-
-    attr_accessor :title,
-                  :image_url,
-                  :url, # TODO - make me consistent when the Mondo API changes
-                  :background_color,
-                  :body,
-                  :type
-
+    FIELDS = [
+      :title, :image_url, :url, # TODO - make "url" consistent when the Mondo API changes
+      :background_color, :body, :type
+    ]
 
     # temporary fix until the API accepts JSON data in the request body
     def save
       self.client.api_post("/feed", self.create_params)
     end
 
+    private
+
     def create_params
       {
+        type:       "basic",
         account_id: self.client.account_id,
-        type: "basic",
-        url: self.url,
+        url:        self.url,
         params: {
-          title: self.title,
-          image_url: self.image_url,
+          title:            self.title,
+          image_url:        self.image_url,
           background_color: self.background_color,
-          body: self.body
+          body:             self.body
         }
       }
     end
