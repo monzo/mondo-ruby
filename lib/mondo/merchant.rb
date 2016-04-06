703861840c97a9c0b2fac889447ed3134935baa5
Bilawal Hameed <bilawal@studenthack.com>
2016-04-06 11:52:06 +0100

    factorised Merchant

--- lib/mondo/merchant.rb
+++ lib/mondo/merchant.rb
@@ -1,15 +1,17 @@
 module Mondo
   class Merchant < Resource
+    FIELDS = [
+      :id, :group_id, :logo,
+      :name, :address, :emoji,
+      :raw_data
+    ]
 
-    attr_accessor :id, :group_id, :logo, :name, :raw_data, :address, :emoji
-
+    attr_accessor    *FIELDS
     boolean_accessor :online, :is_load, :settled
-
-    date_accessor :created
+    date_accessor    :created
 
     def address
       ::Mondo::Address.new(raw_data['address'], self.client)
     end
-
   end
 end
